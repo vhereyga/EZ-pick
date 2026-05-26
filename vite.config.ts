6b4f9d1f41ac0f01374add4296056ba3c7966d5c
vhereyga <vhereygaputra@gmail.com>
2026-05-26 23:02:49 +0700

    perbaikan

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/EZ-pick/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
